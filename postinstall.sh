3f2b6da90f7f4b7e4fc16580b4a77b3f8363da2e
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-30 13:24:00 -0600

    add postinstall script

---
+++ postinstall.sh
@@ -0,0 +1,16 @@
+$FOLDER="frontend"
+$URL="https://github.com/orlandodiaz/redux-auth.git"
+if [ ! -d "$FOLDER" ] ; then
+    git clone $URL $FOLDER
+else
+    cd "$FOLDER"
+    git pull $URL
+fi
+
+cd frontend
+npm run build
+
+
+# git clone https://github.com/orlandodiaz/redux-auth.git frontend
+# cd frontend
+#npm run build
