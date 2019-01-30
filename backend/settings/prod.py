9baa74e47d927c2b8e9c21452ec570037b4b352a
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-30 10:18:41 -0600

    change basedir

--- backend/settings/prod.py
+++ backend/settings/prod.py
@@ -13,7 +13,7 @@ DATABASES['default'] = dj_database_url.config(
     conn_max_age=600, ssl_require=True)
 
 # Static FILES
-STATIC_ROOT = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static/")
 
 MIDDLEWARE += [
     'whitenoise.middleware.WhiteNoiseMiddleware',
