36d8f46c05685800af19b2e3a2cdf4ef1cbd8f2b
Orlando Reategui Diaz <orlandordiaz@gmail.com>
2019-01-30 02:05:01 -0600

    Add base, prod, Procfile, and req for deployment

---
+++ backend/settings/prod.py
@@ -0,0 +1,20 @@
+from .base import *
+
+import dj_database_url
+
+DEBUG = True
+
+# Add your domain to allowed hosts:
+ALLOWED_HOSTS = [
+    'orr-redux-auth2.herokuapp.com',
+]
+
+DATABASES['default'] = dj_database_url.config(
+    conn_max_age=600, ssl_require=True)
+
+# Static FILES
+STATIC_ROOT = "static/"
+
+MIDDLEWARE += [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+]
